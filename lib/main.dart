c34b4d1716982ff01da5c56b316e2b70581074ef
Do Nhat <dmnhat.pt@gmail.com>
2024-01-22 12:04:59 +0700

    Building the Grocery List UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/grocery_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
+      home: const GroceryListScreen(),
     );
   }
 }
