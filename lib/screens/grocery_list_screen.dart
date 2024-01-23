d2039169cff6861dd98391b1b297cda9df5a9136
Do Nhat <dmnhat.pt@gmail.com>
2024-01-23 11:33:52 +0700

    Sending DELETE request.

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -73,10 +73,22 @@ class _GroceryListScreenState extends State<GroceryListScreen> {
     }
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https(
+      'flutter-shopping-list-77942-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
